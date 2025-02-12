4f8cd9989e4b84d4f5c6501349e78e8968fe8520
Arpina1 <nazifeburcu.acikoz@stonybrook.edu>
2025-02-12 13:57:45 -0500

    Updated JobSearchDTO

--- server/src/main/java/com/example/job_portal/dto/JobSearchDTO.java
+++ server/src/main/java/com/example/job_portal/dto/JobSearchDTO.java
@@ -1,9 +1,7 @@
 package com.example.job_portal.dto;
 
 import com.example.job_portal.model.JobType;
-import lombok.Data;
 
-@Data
 public class JobSearchDTO {
     private String keyword;
     private String location;
@@ -14,4 +12,97 @@ public class JobSearchDTO {
     private String sortDirection = "DESC"; // default sort direction
     private int page = 0;
     private int size = 10;
+
+    // Default constructor
+    public JobSearchDTO() {
+    }
+
+    // Getters and Setters
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(Double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public Double getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(Double maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public JobType getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(JobType jobType) {
+        this.jobType = jobType;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public String getSortDirection() {
+        return sortDirection;
+    }
+
+    public void setSortDirection(String sortDirection) {
+        this.sortDirection = sortDirection;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    // toString method for debugging
+    @Override
+    public String toString() {
+        return "JobSearchDTO{" +
+                "keyword='" + keyword + '\'' +
+                ", location='" + location + '\'' +
+                ", minSalary=" + minSalary +
+                ", maxSalary=" + maxSalary +
+                ", jobType=" + jobType +
+                ", sortBy='" + sortBy + '\'' +
+                ", sortDirection='" + sortDirection + '\'' +
+                ", page=" + page +
+                ", size=" + size +
+                '}';
+    }
 } 
